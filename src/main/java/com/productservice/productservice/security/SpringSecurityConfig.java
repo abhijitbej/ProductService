21b07a5c6568defc6ffb81d3714a404a6519fbe2
abhijitbej <abhijit.bej@gmail.com>
2024-02-05 01:16:36 +0530

    auth

--- src/main/java/com/productservice/productservice/security/SpringSecurityConfig.java
+++ src/main/java/com/productservice/productservice/security/SpringSecurityConfig.java
@@ -20,12 +20,18 @@ public class SpringSecurityConfig {
                         //.requestMatchers("/getProducts").permitAll()
                         .anyRequest().permitAll()
                 )
-                .csrf().disable()
-                .cors().disable()
+                //.csrf().disable()
+                //.cors().disable()
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 // Form login handles the redirect to the login page from the
                 // authorization server filter chain
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
+
+    /*@Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }*/
 }
